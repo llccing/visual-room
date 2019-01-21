0a6400b5438a22d4f072c926cff9c4c1adf0378d
liuchunfeng <liuchunfeng@jd.com>
2019-01-21 15:32:36 +0800

    update 增加iview组件库

--- src/main.js
+++ src/main.js
@@ -1,23 +1,28 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import * as THREE from "three";
-import Stats from 'stats.js'
+import Vue from 'vue';
+import iview from 'iview';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import * as THREE from 'three';
+import Stats from 'stats.js';
 import TWEEN from '@tweenjs/tween.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import 'iview/dist/styles/iview.css';
+
+Vue.use(iview)
 
 window.THREE = THREE;
 window.Stats = Stats;
 window.TWEEN = TWEEN;
 
-require("three/examples/js/controls/OrbitControls")
-require("@/assets/lib/ThreeBSP");
+require('three/examples/js/controls/OrbitControls');
+require('@/assets/lib/ThreeBSP');
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount('#app');
