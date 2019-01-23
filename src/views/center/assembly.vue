dcb2d2d0d6fa4406d6681ec7b799902d089f479e
liuchunfeng <liuchunfeng@jd.com>
2019-01-23 17:13:08 +0800

    update 增加机房信息的存储localstorage

--- src/views/center/assembly.vue
+++ src/views/center/assembly.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="con clearfix">
     <div class="left">
-      <div
-        :class="{'choose': item.choose}"
-        @click="cabinetTypeClick(idx)"
-        class="cabinet"
-        v-for="(item, idx) in cabinetType"
-      >
-        <div>名字:{{item.name}}</div>
-        <div>宽度:{{item.width}}</div>
-        <div>高度:{{item.height}}</div>
-        <div>类型:{{item.type}}</div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">待填充机柜</h3>
+        </div>
+        <div class="panel-body">
+          <div
+            :class="{'choose': item.choose}"
+            @click="cabinetTypeClick(idx)"
+            class="cabinet"
+            v-for="(item, idx) in cabinetType"
+          >
+            <div>名字:{{item.name}}</div>
+            <div>宽度:{{item.width}}</div>
+            <div>高度:{{item.height}}</div>
+            <div>类型:{{item.type}}</div>
+          </div>
+        </div>
       </div>
 
       <button @click.prevent="fillCabinet" class="btn btn-primary">填充机柜</button>
@@ -18,37 +25,79 @@
     <div class="right">
       <div class="row">
         <div class="col-sm-8 room">
-          <div class="cabinet-row clearfix" v-for="(rowItem, rowIdx) in cabinetArr">
+          <div class="cabinet-row clearfix" v-for="(rowItem, rowIdx) in cabinetObj.data">
             <div
               :class="item.classes"
               :style="cabinetColStyle"
               @click="cabinetBoxClick(rowIdx,colIdx)"
               class="cabinet-col"
-              v-for="(item, colIdx) in cabinetArr[rowIdx]"
+              v-for="(item, colIdx) in cabinetObj.data[rowIdx]"
             >
               <div>{{item.info.name}}</div>
             </div>
           </div>
         </div>
+
         <div class="col-sm-4">
-          <form class="form">
-            <p>机房的机柜信息</p>
-            <div class="form-group">
-              <label>机柜排数 &nbsp;</label>
-              <input class="form-control" type="number" v-model="room.cabinetNumber">
+          <div class="room-config">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title">机房方案</h3>
+              </div>
+              <div class="panel-body">
+                <div>
+                  <div
+                    :key="idx"
+                    @click="storeClick(item)"
+                    class="media"
+                    :class="{active: item.id === cabinetObj.id}"
+                    style="cursor: pointer;"
+                    v-for="(item, idx) in storeCenterData"
+                  >
+                    <div class="media-left media-middle">
+                      <img
+                        alt="..."
+                        class="media-object"
+                        src="../../assets/logo.png"
+                        style="width: 32px; height: 32px; "
+                      >
+                    </div>
+                    <div class="media-body">
+                      <h4 class="media-heading">No.{{idx+1}} 机房{{`${item.row}*${item.col}`}}</h4>
+                      {{`我是一个${item.row}行${item.col}列的机房`}}
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
 
-            <div class="form-group">
-              <label>每排个数 &nbsp;</label>
-              <input class="form-control" type="number" v-model="room.count">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">机房的机柜信息</h3>
             </div>
+            <div class="panel-body">
+              <form class="form">
+                <div class="form-group">
+                  <label>机柜排数 &nbsp;</label>
+                  <input class="form-control" type="number" v-model="room.cabinetNumber">
+                </div>
 
-            <div class="form-group">
-              <label>间隔（单位：厘米）</label>
-              <input class="form-control" type="number" v-model="room.padding">
+                <div class="form-group">
+                  <label>每排个数 &nbsp;</label>
+                  <input class="form-control" type="number" v-model="room.count">
+                </div>
+
+                <div class="form-group">
+                  <label>间隔（单位：厘米）</label>
+                  <input class="form-control" type="number" v-model="room.padding">
+                </div>
+
+                <button @click.prevent="initCabinet" class="btn btn-success">生成机柜位置</button>
+                <button @click.prevent="store2Local" class="btn btn-info ml-5">存储方案</button>
+              </form>
             </div>
-            <button @click.prevent="initCabinet" class="btn btn-success">生成机柜位置</button>
-          </form>
+          </div>
         </div>
       </div>
     </div>
@@ -68,6 +117,8 @@ export default {
 
       // 机柜排数
       cabinetArr: [],
+      cabinetObj: {},
+      storeCenterData: [],
 
       // 机柜box样式
       cabinetColStyle: {},
@@ -77,6 +128,7 @@ export default {
   computed: {},
   mounted() {
     this.getAllCabinet();
+    this.getCenterData();
   },
   methods: {
     // 获取所有类型机柜
@@ -107,8 +159,18 @@ export default {
       });
 
       this.cabinetType = data;
-      console.log(data);
     },
+
+    // 获取已经配置好的机房配置信息
+    getCenterData() {
+      let centerData = JSON.parse(localStorage.getItem("centerData") || []);
+      if (centerData.length > 0) {
+        this.storeCenterData = centerData;
+        this.cabinetObj = centerData[0];
+        this.calcStyle(centerData[0].col)
+      }
+    },
+
     // 生成待填充机柜
     initCabinet() {
       let row = [];
@@ -128,28 +190,28 @@ export default {
         }
       }
 
-      this.cabinetArr = row;
+      this.cabinetObj.data = row;
 
-      this.calcStyle();
+      this.calcStyle(row[0].length);
     },
 
     // 计算样式
-    calcStyle() {
+    calcStyle(roomCount) {
       let style = {};
       /**
        * 用90%的宽度去给每个机柜分配宽度
        * 用剩下的10%的宽度给你每个机柜设置左右边距，这样就能够居中显示。
        */
-      style.width = `${(0.9 / this.room.count) * 100}%`;
+      style.width = `${(0.9 / roomCount) * 100}%`;
       style.height = `${style.width * 2}px`;
-      style.margin = `0 ${(0.1 / 2 / this.room.count) * 100}% 5px`;
+      style.margin = `0 ${(0.1 / 2 / roomCount) * 100}% 5px`;
       this.cabinetColStyle = style;
     },
 
     // 机柜box点击事件
     cabinetBoxClick(rowIdx, colIdx) {
-      let item = this.cabinetArr[rowIdx][colIdx];
-      if(item.filled){
+      let item = this.cabinetObj.data[rowIdx][colIdx];
+      if (item.filled) {
         return;
       }
       item.choose = !item.choose;
@@ -160,7 +222,6 @@ export default {
 
     // 填充机柜
     fillCabinet() {
-      console.log(11);
       // 被选中的设备
       let chooseItem;
       let flag = false;
@@ -179,20 +240,42 @@ export default {
         return;
       }
 
-      console.log(chooseItem);
-
-      this.cabinetArr.forEach(rowItem => {
+      this.cabinetObj.data.forEach(rowItem => {
         rowItem.forEach(item => {
           if (item.classes.choose) {
             item.info = chooseItem;
             item.filled = true;
             item.classes.choose = false;
-
-            console.log(item);
           }
         });
-        
       });
+    },
+
+    store2Local() {
+      let data = JSON.parse(JSON.stringify(this.cabinetObj.data));
+      if (data.length === 0) {
+        this.$Notice.warning({
+          title: "不能添加空数据！"
+        });
+        return;
+      }
+
+      let obj = {
+        id: new Date().getTime(),
+        row: this.cabinetObj.data.length,
+        col: this.cabinetObj.data[0].length,
+        data: data
+      };
+
+      let centerData = JSON.parse(localStorage.getItem("centerData")) || [];
+      if (centerData.length === 0) {
+        localStorage.setItem("centerData", JSON.stringify([obj]));
+      } else {
+        centerData = centerData.concat([obj]);
+        localStorage.setItem("centerData", JSON.stringify(centerData));
+      }
+
+      this.getCenterData();
     },
 
     cabinetTypeClick(idx) {
@@ -204,18 +287,28 @@ export default {
 
       let item = this.cabinetType[idx];
       item.choose = !item.choose;
+    },
+
+    storeClick(item) {
+      this.cabinetObj = item;
+      this.calcStyle(item.col);
     }
   }
 };
 </script>
 
 <style lang="less" scoped>
+.ml-5 {
+  margin-left: 5px;
+}
+
 .con {
   padding: 10px;
 
   .left {
     float: left;
     width: 200px;
+    padding-right: 15px;
 
     .cabinet {
       width: 100px;
@@ -234,7 +327,6 @@ export default {
 
   .right {
     margin-left: 210px;
-    // background: yellow;
 
     .room {
       border: 1px solid #f00;
@@ -267,6 +359,17 @@ export default {
         .style-c {
           background: #f5f;
         }
+      }
+    }
+
+    .room-config{
+      .media{
+        margin: -5px -15px;
+        padding: 5px 15px;
+      }
+      
+      .active{
+        background: #53b79e38;
       }
     }
 
