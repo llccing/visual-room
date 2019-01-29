4784d4ee5cd4deae1f54169cc781a66588536cbb
liuchunfeng <liuchunfeng@jd.com>
2019-01-29 17:35:55 +0800

    update 增加GitHub pages配置

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+const githubPath = 'https://github.com/llccing/visual-room/blob/gh-pages/';
+// 使用域名之后的地址
+const hostPath = 'http://blog.llccing.cn/visual-room/'
+
+const BASE_URL = process.env.NODE_ENV === 'production' ? hostPath : '/'
+
+module.exports = {
+  baseUrl: BASE_URL,
+  productionSourceMap: false,
+}
