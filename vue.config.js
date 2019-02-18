130b1401a9dca0cd71271943067c383ee7b22d75
liuchunfeng <liuchunfeng@jd.com>
2019-02-18 16:55:38 +0800

    update 修改host

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const githubPath = 'https://github.com/llccing/visual-room/blob/gh-pages/';
 // 使用域名之后的地址
-const hostPath = 'http://blog.llccing.cn/visual-room/'
+const hostPath = 'http://gh.llccing.cn/visual-room/'
 
 const BASE_URL = process.env.NODE_ENV === 'production' ? hostPath : '/'
 
